62eec79479a773df7c558d4d4c9c236c7d68ca8f
NaMinhyeok <nmh9097@gmail.com>
2025-08-06 19:18:25 +0900

    Initial Commit

---
+++ src/main/java/org/nmh/study/DynamicProxyPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.nmh.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicProxyPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicProxyPracticeApplication.class, args);
+    }
+
+}
